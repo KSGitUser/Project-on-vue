ae68d853cd2a28481d82ca3d1b27aac6f969bd98
KSFirstUser <rusakk@mail.ru>
2019-06-13 01:58:12 +0300

    install vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
@@ -11,4 +12,5 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
