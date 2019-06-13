9abc08c54b389319feb39731749caa4c0b706e1a
KSFirstUser <rusakk@mail.ru>
2019-06-13 18:31:53 +0300

    connect to firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import * as fb from 'firebase/app' // firebase connect
 
 Vue.config.productionTip = false
 
@@ -13,4 +14,18 @@ new Vue({
   render: h => h(App),
   router,
   store,
+  created() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCszcNIqcIHvA2eAg3viIBrAHkO4fX-8ro",
+      authDomain: "online-store-on-vue.firebaseapp.com",
+      databaseURL: "https://online-store-on-vue.firebaseio.com",
+      projectId: "online-store-on-vue",
+      storageBucket: "online-store-on-vue.appspot.com",
+      messagingSenderId: "100773435953",
+      appId: "1:100773435953:web:49c67a364ba588a1"
+    }
+    // Initialize Firebase
+    fb.initializeApp(firebaseConfig);
+  }
 }).$mount('#app')
